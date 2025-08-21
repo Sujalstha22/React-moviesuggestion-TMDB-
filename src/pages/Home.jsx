25100d1b40719fbd950e711d929818d72109b58d
Sujal Shrestha <shresthasujal309@gmail.com>
2025-08-21 19:09:29 +0545

    fixed the search error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,11 @@ const Home = ({ searchQuery }) => {
   const [movies, setmovies] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+
   const filteredMovies = movies.filter((movie) =>
     movie.title.toLowerCase().includes(searchQuery.toLowerCase())
   );
+
   useEffect(() => {
     const loadPopularMovies = async () => {
       try {
@@ -23,25 +25,20 @@ const Home = ({ searchQuery }) => {
     };
 
     loadPopularMovies();
-  }, [movies]);
+  }, []); // run only once
 
   return (
     <div className="min-h-screen bg-gray-900 text-white px-6 py-10">
       <h1 className="text-3xl font-bold text-center text-yellow-400 mb-10">
         Movie Collection
       </h1>
-      {error && <div className=""> {error} </div>}
+
+      {error && <div className="text-red-500 text-center mb-4">{error}</div>}
+
       {loading ? (
-        <div className="">Loading..</div>
-      ) : (
+        <div className="text-center">Loading..</div>
+      ) : filteredMovies.length > 0 ? (
         <div className="grid gap-x-1 gap-y-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          {movies.map((movie) => (
-            <Moviecard movie={movie} key={movie.id} />
-          ))}
-        </div>
-      )}
-      {filteredMovies.length > 0 ? (
-        <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {filteredMovies.map((movie) => (
             <Moviecard movie={movie} key={movie.id} />
           ))}
